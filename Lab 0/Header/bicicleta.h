b585b36bc5542f13781595e752c4262261fc94de
Juan Ferrand <JuanFerrand@users.noreply.github.com>
2019-03-12 13:22:00 -0300

    prep

---
+++ Lab 0/Header/bicicleta.h
@@ -0,0 +1,30 @@
+#ifndef BICICLETA_H
+#define BICICLETA_H
+
+#include "vehiculo.h"
+#include "DtBicicleta.h"
+
+
+class Bicicleta{
+	private:
+		TipoBici tipo;
+		int cantCambios;
+	public:	
+		//Constructor
+		Bicicleta(TipoBici tipo, int cantCambios);
+
+		//Get
+		TipoBici getTipo();
+		int getcantCambios();
+
+		//Set
+		void setTipo(TipoBici tipo);
+		void setcantCambios(int cantCambios);
+
+		//Def
+		float darPrecioViaje(int duracion, int distancia);
+}
+
+
+
+#endif
